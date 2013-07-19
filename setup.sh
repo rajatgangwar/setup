a368fe2603a33f056af267712d549b8257050073
Ubuntu <ubuntu@ip-172-31-24-129.us-west-2.compute.internal>
2013-07-19 17:54:55 +0000

    heorku install

--- setup.sh
+++ setup.sh
@@ -47,3 +47,6 @@ sudo apt-get install -y python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install -y nodejs
+
+
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
